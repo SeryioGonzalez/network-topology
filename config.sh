e0fb4a3e51b8ac8caca2efea217f0d9d7ea31ae7
seryio.gonzalez <sergio.gonzalez.jimenez@gmail.com>
2018-12-02 01:50:28 +0100

    vms

--- config.sh
+++ config.sh
@@ -1,12 +1,14 @@
 #/bin/bash
 
-resGroupPrefix="poc"
+resGroupPrefix="gvp"
 
 location="westeurope"
 
 coreVnetName="core_vnet"
 coreVnetParameterFile="parameters_core_vnet.json"
 coreVnetTemplateFile="template_core_vnet.json"
+coreVnetDeploymentName="template_core_vnet"
+coreVnetResGroupName=$resGroupPrefix"_vnet_core"
 
 firewallCoreVnetVpnsTemplate="template_firewall_core_vnet_vpns.json"
 firewallCoreVnetVpnsParameters="parameters_firewall_core_vnet_vpns.json"
@@ -14,3 +16,6 @@ firewallCoreVnetVpnsParameters="parameters_firewall_core_vnet_vpns.json"
 spokeDataFile="data_spoke_vnets.json"
 regularSpokeVnetTemplateFile="template_regular_spoke_vnet.json"
 natSpokeVnetTemplateFile="template_nat_spoke_vnet.json"
+
+vmssTemplate="template_vmss.json"
+vmssParameters="parameters_vmss.json"
